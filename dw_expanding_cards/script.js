ebcb92dca59e6b63a84db86bd0b96ec626ff0b6e
Joyce Z <yz4867@nyu.edu>
2023-10-07 21:43:35 -0400

    card display & interactive

---
+++ dw_expanding_cards/script.js
@@ -0,0 +1,16 @@
+//select the panel
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach(panel => {
+  panel.addEventListener('click', () => {
+    removeActiveClasses()
+    panel.classList.add('active')
+    console.log("clicked")
+  })
+})
+
+function removeActiveClasses() {
+  panels.forEach(panel => {
+    panel.classList.remove('active')
+  })
+}
